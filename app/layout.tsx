7945c3cb02a5877cb71b02087cf41ab4fa1feeac
Gerison Kimathi <gerkim62@gmail.com>
2023-09-27 21:20:44 +0300

    ggg

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import { Inter } from "next/font/google";
 
 import { Sidebar } from "./components/Sidebar";
 import { Footer } from "./components/Footer";
-import { AuthProvider } from "./Providers";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
